876cafd6ee153d88bd18dc69a2d87e56f78de666
JorgeLiang <276224495@qq.com>
2022-11-21 14:50:04 +0800

    test 2

--- android/src/main/java/com/tailscale/ipn/App.java
+++ android/src/main/java/com/tailscale/ipn/App.java
@@ -97,7 +97,7 @@ public class App extends Application {
 		createNotificationChannel(STATUS_CHANNEL_ID, "VPN Status", NotificationManagerCompat.IMPORTANCE_LOW);
 		createNotificationChannel(FILE_CHANNEL_ID, "File transfers", NotificationManagerCompat.IMPORTANCE_DEFAULT);
 
-		Toast.makeText(BaseActivity.this, "TS 测试", duration).show();
+		Toast.makeText(BaseActivity.this, "TS 测试1", Toast.LENGTH_LONG).show();
 	}
 
 	private void registerNetworkCallback() {

7f1ea22c9b663e6dd84f8bd8768ebf5009772899
JorgeLiang <276224495@qq.com>
2022-11-21 16:13:01 +0800

    test 10

---
+++ android/src/main/java/com/tailscale/jlog/tslog/StringUtils.java
@@ -0,0 +1,30 @@
+package com.tailscale.jlog.tslog;
+
+/**
+ * Created by Jorge on 4/29/21.
+ */
+
+public class StringUtils {
+
+    /**
+     * @param beginStr  "_"
+     * @param endStr    "."
+     * @param totalStr  AAA_1.txt
+     * @return          1
+     */
+    public static String cutMiddleString(String beginStr,String endStr,String totalStr){
+        String cutFinish = "";
+        if (totalStr.contains(beginStr) && totalStr.contains(endStr)){
+//            int beginIndex = totalStr.indexOf(beginStr);
+            int beginIndex = totalStr.lastIndexOf(beginStr);
+            int lastIndex = totalStr.lastIndexOf(endStr);
+
+            int begin = beginIndex + beginStr.length();
+
+            cutFinish = totalStr.substring(begin,lastIndex);
+        }
+
+        return cutFinish.isEmpty() ? totalStr : cutFinish;
+    }
+
+}
